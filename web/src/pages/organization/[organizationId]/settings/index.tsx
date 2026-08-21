70d4eb732c26e340525f60399ab518569e3227d5
Jeffrey <color.dove@gmail.com>
2026-08-21 10:36:27 +0800

    Remove AI feature switch from organization settings

--- web/src/pages/organization/[organizationId]/settings/index.tsx
+++ web/src/pages/organization/[organizationId]/settings/index.tsx
@@ -12,7 +12,6 @@ import { useHasEntitlement } from "@/src/features/entitlements/hooks";
 import ContainerPage from "@/src/components/layouts/container-page";
 import { useQueryProjectOrOrganization } from "@/src/features/projects/hooks";
 import { ApiKeyList } from "@/src/features/public-api/components/ApiKeyList";
-import AIFeatureSwitch from "@/src/features/organizations/components/AIFeatureSwitch";
 import { env } from "@/src/env.mjs";
 
 // EE features removed from OSS build:
@@ -67,7 +66,6 @@ export const getOrganizationSettingsPages = ({
             }}
           />
         </div>
-        <AIFeatureSwitch />
         <SettingsDangerZone
           items={[
             {
